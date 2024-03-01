4e2a102887887ff4d4b2b5e921872220e0715a84
JoeDiaco <joe@joediaco.com>
2024-03-01 12:50:02 -0500

    many things

--- utils/filters.js
+++ utils/filters.js
@@ -8,5 +8,10 @@ module.exports = {
 		return DateTime.fromJSDate(dateObj, {
 			zone: 'utc'
 		}).toFormat('yyyy-LL-dd');
+	},
+	htmlYearString: (dateObj) => {
+		return DateTime.fromJSDate(dateObj, {
+			zone: 'utc'
+		}).toFormat('yyyy');
 	}
 }
