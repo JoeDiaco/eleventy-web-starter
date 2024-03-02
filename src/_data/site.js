afbc1e7e55f37d4ecbd89cf9e87bd18f754cfecf
JoeDiaco <joe@joediaco.com>
2024-03-01 20:21:59 -0500

    many things

--- src/_data/site.js
+++ src/_data/site.js
@@ -8,5 +8,26 @@ module.exports = {
 		"twitter": "@chrissy_dev",
 		"opengraph_default": "/static/opengraph-default.jpg"
 	},
-	"env": process.env.ELEVENTY_ENV === 'production'
+	"env": process.env.ELEVENTY_ENV === 'production',
+	"navigationOptions":
+	{
+		"navbar": {
+			listElement: "ul",            // Change the top level tag
+			listItemElement: "li",        // Change the item tag
+
+			listClass: "flex flex-col space-y-4 lg:mt-0 lg:flex-row lg:space-y-0",
+			anchorClass: "text-gray-700 lg:mx-6 dark:text-gray-200 dark:hover:text-blue-400 hover:text-blue-500",              // Add a class to the anchor
+			activeAnchorClass: "",        // Add a class to the current page’s anchor
+			showExcerpt: false
+		},
+		"footer": {
+			listElement: "ul",            // Change the top level tag
+			listItemElement: "li",        // Change the item tag
+
+			listClass: "flex flex-wrap justify-center mt-6 -mx-4",
+			anchorClass: "mx-4 text-sm text-gray-600 transition-colors duration-300 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-400",              // Add a class to the anchor
+			activeAnchorClass: "",        // Add a class to the current page’s anchor
+			showExcerpt: false
+		}
+	}
 }
