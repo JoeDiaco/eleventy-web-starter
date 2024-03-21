c851933a39a917fecfbba029c438d197e32b9f38
JoeDiaco <joe@joediaco.com>
2024-03-21 17:56:52 -0400

    stuff

--- src/_assets/scripts/app.js
+++ src/_assets/scripts/app.js
@@ -3,3 +3,14 @@ window.Alpine = Alpine
 Alpine.start()
 
 console.log(`I was loaded at ${Date(Date.now()).toString()}`);
+
+const elSearchBar = document.querySelector("#searchBar")
+console.log(elSearchBar)
+const elResponseArea = document.querySelector("#responseArea");
+document
+    .querySelector("#searchButton")
+    .addEventListener("click", async (event) => {
+        let response = await fetch(`https://totalpillowchad.com/.netlify/functions/test-function?name=${elSearchBar.textContent}`)
+        let message = response.json();
+        console.log(message)
+    })
