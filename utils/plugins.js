07ef4115a55e6a4210471d2167a08b77ed2856b9
JoeDiaco <joe@joediaco.com>
2024-03-29 18:16:22 -0400

    added noopener plugin

--- utils/plugins.js
+++ utils/plugins.js
@@ -1,3 +1,6 @@
 module.exports = {
-    eleventyNavigationPlugin: require("@11ty/eleventy-navigation")
+    eleventyNavigationPlugin: require("@11ty/eleventy-navigation"),
+    automaticNoopener: require('eleventy-plugin-automatic-noopener'),
+
 }
+
